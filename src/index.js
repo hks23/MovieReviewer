c58ac27bb9d252a8068bb2cd4a32d66666a1f721
Harsh Kr Singh <harshks.mit@gmail.com>
2024-08-19 18:27:12 +0530

    final project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <StarRating
+      maxRating={5}
+      className="test"
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+      defaultRating={3}
+    />
+    <Test /> */}
+    <App />
+  </React.StrictMode>
+);
